8ed3bca56aff71134541a9cf0183880d899eed52
pjanov <pjanov84@mail.ru>
2023-10-19 22:04:18 +0300

    Added an employee class

---
+++ src/main/java/ru/pyanov/Employee.java
@@ -0,0 +1,39 @@
+package ru.pyanov;
+
+public class Employee {
+    private int employeeNumber;
+    private String phoneNumber;
+    private String name;
+    private int workExperience;
+
+    /**
+     * Конструктор
+     *
+     * @param employeeNumber Табельный номер
+     * @param phoneNumber    Номер телефона
+     * @param name           Имя
+     * @param workExperience Стаж работы
+     */
+    public Employee(int employeeNumber, String phoneNumber, String name, int workExperience) {
+        this.employeeNumber = employeeNumber;
+        this.phoneNumber = phoneNumber;
+        this.name = name;
+        this.workExperience = workExperience;
+    }
+
+    public int getEmployeeNumber() {
+        return employeeNumber;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getWorkExperience() {
+        return workExperience;
+    }
+}

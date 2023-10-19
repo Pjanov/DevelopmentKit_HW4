de9ec09761846f3e5398a9d0a5930ba32fab8393
pjanov <pjanov84@mail.ru>
2023-10-19 22:37:34 +0300

    Added the Employee directory class

---
+++ src/main/java/ru/pyanov/EmployeeDirectory.java
@@ -0,0 +1,72 @@
+package ru.pyanov;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class EmployeeDirectory {
+    private List<Employee> employees;
+
+    public EmployeeDirectory() {
+        employees = new ArrayList<>();
+    }
+
+    /**
+     * Добавить сотрудника
+     *
+     * @param employee сотрудник
+     */
+    public void addEmployee(Employee employee) {
+        employees.add(employee);
+    }
+
+    /**
+     * Найти сотрудника по стажу
+     *
+     * @param workExperience Стаж
+     * @return Список сотрудников
+     */
+    public List<Employee> findEmployeeByExperience(int workExperience) {
+        List<Employee> employeeList = new ArrayList<>();
+        for (Employee employee : employees) {
+            if (employee.getWorkExperience() == workExperience) {
+                employees.add(employee);
+            }
+        }
+        return employeeList;
+    }
+
+    /**
+     * Найти номер телефона по имени
+     *
+     * @param name имя
+     * @return Список номеров
+     */
+    public List<String> findPhoneNumberByName(String name) {
+        List<String> foundPhoneNumbers = new ArrayList<>();
+        for (Employee employee : employees) {
+            if (employee.getName().equals(name)) {
+                foundPhoneNumbers.add(employee.getPhoneNumber());
+            }
+        }
+        return foundPhoneNumbers;
+    }
+
+    /**
+     * Найти сотрудника по табельному номеру
+     *
+     * @param serviceNumber табельный номер
+     * @return Сотрудника
+     */
+    public Employee findEmployeeByServiceNumber(int serviceNumber) {
+        for (Employee employee : employees) {
+            if (employee.getEmployeeNumber() == serviceNumber) {
+                return employee;
+            }
+        }
+        return null;
+    }
+
+    public List<Employee> getEmployees() {
+        return employees;
+    }
+}
